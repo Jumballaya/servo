0bd4d87ee035a1a20d86739fd2f4931c0868fcb8
Patrick Burris <nchupacabra@gmail.com>
2018-07-13 13:24:06 -0500

    Add history to the REPL

--- repl/repl.go
+++ repl/repl.go
@@ -13,9 +13,53 @@ import (
 
 const PROMPT = ">> "
 
+type History struct {
+	Current int
+	History []string
+}
+
+func NewHistory() *History {
+	return &History{Current: 0, History: []string{}}
+}
+
+func (h *History) Back() {
+	length := len(h.History)
+	if h.Current < 0 || h.Current > length-1 {
+		h.Current = 0
+		return
+	}
+
+	h.Current--
+}
+
+func (h *History) Forward() {
+	length := len(h.History)
+	if h.Current < 0 {
+		h.Current = 0
+		return
+	}
+	if h.Current > length-1 {
+		h.Current = length - 1
+		return
+	}
+
+	h.Current += 1
+}
+
+func (h *History) CurrentInput() string {
+	return h.History[h.Current]
+}
+
+func (h *History) Insert(command string) {
+	h.History[h.Current] = command
+	h.Current = len(h.History) + 1
+	h.History = append(h.History, "")
+}
+
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := stdlib.NewEnvironmentWithLib()
+	history := NewHistory()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -25,6 +69,8 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
+		history.Insert(line)
+
 		l := lexer.New(line)
 		p := parser.New(l)
 
